5699a68565f9d77d0c1f0fd56971963af4edeee6
heyiming <ymjrcc@qq.com>
2024-09-03 15:46:54 +0800

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function Page() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
+      <h1 className="text-3xl font-bold underline text-blue-600 text-center p-8">
+        Hello Tailwindcss!
+      </h1>
+    </>
   );
 }
 
