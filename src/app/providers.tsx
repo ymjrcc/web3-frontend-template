43713d8f2553df7151afba7be698dd9032509453
heyiming <ymjrcc@qq.com>
2024-11-19 14:07:52 +0800

    add branch without web3

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,22 +2,14 @@
 
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import {NextUIProvider} from '@nextui-org/react'
-
-import { config } from '../wagmi';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider locale='en-US'>
-          <NextUIProvider>{children}</NextUIProvider>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </QueryClientProvider>
   );
 }
